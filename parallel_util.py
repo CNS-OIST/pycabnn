1aa1f5d832de13a3fd1795408a9c3d9ab27041b0
Sungho@macpro <shhong@oist.jp>
2017-12-18 19:19:44 +0900

    Make some changes according to PEP8, use pathlib, and add pseudo parallel function to check empty files.

--- parallel_util.py
+++ parallel_util.py
@@ -1,32 +1,31 @@
-
-
-def str_l (ar): 
+def str_l(ar):
     '''make a space-seperated string from all elements in a 1D-array'''
     return (' '.join(str(ar[i]) for i in range(len(ar))))
 
 
-def find_connections_2dpar (kdt, pts, lpts, c_rad, lin_axis, lin_in_tree, lin_is_src, ids, prefix):
-    
+def find_connections_2dpar(kdt, pts, lpts, c_rad, lin_axis, lin_in_tree, lin_is_src, ids, prefix):
+
     import numpy
 
-    if lin_in_tree: q_pts = pts.coo[:, numpy.invert(lin_axis)]  
-    else: q_pts = lpts.coo[:,0,numpy.invert(lin_axis)]
+    if lin_in_tree:
+        q_pts = pts.coo[:, numpy.invert(lin_axis)]
+    else:
+        q_pts = lpts.coo[:, 0 ,numpy.invert(lin_axis)]
 
-    lax_c = pts.coo[:,lin_axis] 
-    lax_range = lpts.coo[:,:,lin_axis] 
+    lax_c = pts.coo[:, lin_axis]
+    lax_range = lpts.coo[:, : ,lin_axis]
     lax_range = lax_range.reshape((lax_range.shape[0], lax_range.shape[1]))
-
-
 
     res = []
     res_l = []
 
-    for i, pt in enumerate(q_pts[ids[1]]): #iterate through the query points
+    #iterate through the query points
+    for i, pt in enumerate(q_pts[ids[1]]):
         # find the points within the critical radius
-        ind, = kdt.query_radius(numpy.expand_dims(pt, axis = 0), r = c_rad)
+        ind, = kdt.query_radius(numpy.expand_dims(pt, axis=0), r=c_rad)
 
         #check if the found points match along the linearized axis and if so, add distance from the beginning of the linearized axis
-        if lin_in_tree: 
+        if lin_in_tree:
             ind = ind[numpy.logical_and(lax_range[ind,0]<=lax_c[i], lax_range[ind,1]>= lax_c[i])]
             res_l.append(abs(lax_c[i] - lax_range[ind,0] -lpts.set_0) + lpts.lin_offset[ind])
         else:
@@ -35,37 +34,35 @@ def find_connections_2dpar (kdt, pts, lpts, c_rad, lin_axis, lin_in_tree, lin_is
 
         res.append(ind.astype('int'))
 
-    #print (len(pts.seg))
+    # print (len(pts.seg))
 
-
-
+    prefix = str(prefix)
     fn_tar = prefix + 'targets' + str(ids[0])+'.dat'
     fn_src = prefix + 'sources' + str(ids[0])+'.dat'
-    fn_segs = prefix +'segments'+ str(ids[0])+'.dat'
-    fn_dis = prefix + 'distances'+ str(ids[0])+'.dat'
+    fn_segs = prefix + 'segments' + str(ids[0])+'.dat'
+    fn_dis = prefix + 'distances' + str(ids[0])+'.dat'
 
-
-    with open (fn_tar, 'w') as f_tar, open (fn_src, 'w') as f_src, open (fn_dis, 'w') as f_dis, open (fn_segs, 'w') as f_segs: 
+    with open(fn_tar, 'w') as f_tar, open(fn_src, 'w') as f_src, open(fn_dis, 'w') as f_dis, open(fn_segs, 'w') as f_segs:
 
         for (l, cl, cl_l) in zip(list(ids[1]), res, res_l):
-            
-            assert len(cl) == len(cl_l), 'Something went wrong, all corresponding lists in your input arguments should have the same length'               
+
+            assert len(cl) == len(cl_l), 'Something went wrong, all corresponding lists in your input arguments should have the same length'
 
             if len(cl_l)>0:
                 f_dis.write("\n".join(map(str, cl_l)))
-                #first, get the cell IDS of the query and tree points (for the linear points, that is just the point ID, 
+                #first, get the cell IDS of the query and tree points (for the linear points, that is just the point ID,
                 #for the other points this information has to be extracted from the corresponding Query_points object.
                 #Segments also corresponds to the 3D point population, right value is acquired from Query-points object.
-                if lin_in_tree: 
+                if lin_in_tree:
                     s_ar = pts.seg[l,:].astype('int')
-                    f_segs.write("\n".join(map(str_l, [s_ar for i in range (len(cl))])))#*numpy.ones((len(cl), len (s_ar)))))) 
+                    f_segs.write("\n".join(map(str_l, [s_ar for i in range (len(cl))])))#*numpy.ones((len(cl), len (s_ar))))))
 
                     q_id = numpy.ones(len(cl))*pts.idx[l]
                     tr_id = cl
                 else:
                     f_segs.write("\n".join(map(str_l, pts.seg[cl].astype('int'))))
-                    q_id = pts.idx[cl] 
-                    tr_id = numpy.ones(len(cl))*l 
+                    q_id = pts.idx[cl]
+                    tr_id = numpy.ones(len(cl))*l
 
                 #depending on which population should be source and which should be target, save cell IDs accordingly.
                 if lin_in_tree == lin_is_src:
@@ -75,44 +72,43 @@ def find_connections_2dpar (kdt, pts, lpts, c_rad, lin_axis, lin_in_tree, lin_is
                     f_tar.write("\n".join(map(str, q_id)))
                     f_src.write("\n".join(map(str, tr_id )))
 
-                #need to attach one more line here or we get two elements per line 
+                #need to attach one more line here or we get two elements per line
                 f_dis.write("\n")
                 f_src.write("\n")
                 f_tar.write("\n")
                 f_segs.write("\n")
 
+    # return [ids[0], res, res_l]
 
-    return [ids[0], res, res_l]
 
+def find_connections_3dpar(kdt, spts, tpts, c_rad,  src_in_tree, ids, prefix):
 
-def find_connections_3dpar (kdt, spts, tpts, c_rad,  src_in_tree, ids, prefix):
-    
     import numpy
 
-    if src_in_tree: q_pts = tpts.coo 
-    else: q_pts = spts.coo
+    if src_in_tree:
+        q_pts = tpts.coo
+    else:
+        q_pts = spts.coo
 
     res = []
 
-    for i, pt in enumerate(q_pts[ids[1]]): #iterate through the query points
+    # iterate through the query points
+    for i, pt in enumerate(q_pts[ids[1]]):
         # find the points within the critical radius
-        ind, = kdt.query_radius(numpy.expand_dims(pt, axis = 0), r = c_rad)
+        ind, = kdt.query_radius(numpy.expand_dims(pt, axis=0), r=c_rad)
         res.append(ind.astype('int'))
 
-    
+    prefix = str(prefix)
 
+    fn_tar = prefix + 'target' + str(ids[0]) + '.dat'
+    fn_src = prefix + 'source' + str(ids[0]) + '.dat'
+    fn_sseg = prefix + 'source_segments' + str(ids[0]) + '.dat'
+    fn_tseg = prefix + 'target_segments' + str(ids[0]) + '.dat'
 
-
-    fn_tar = prefix + 'target' + str(ids[0])+'.dat'
-    fn_src = prefix + 'source' + str(ids[0])+'.dat'
-    fn_sseg = prefix +'source_segments'+ str(ids[0])+'.dat'
-    fn_tseg = prefix + 'target_segments'+ str(ids[0])+'.dat'
-
-
-    with open (fn_tar, 'w') as f_tar, open (fn_src, 'w') as f_src, open (fn_sseg, 'w') as f_sseg, open (fn_tseg, 'w') as f_tseg: 
+    with open(fn_tar, 'w') as f_tar, open(fn_src, 'w') as f_src, open(fn_sseg, 'w') as f_sseg, open(fn_tseg, 'w') as f_tseg:
 
         for (l, cl) in zip(list(ids[1].astype('int')), res):
-            
+
             if len(cl)>0:
                 #depending on which population should be source and which should be target, save cell IDs accordingly.
                 if src_in_tree:
@@ -127,17 +123,10 @@ def find_connections_3dpar (kdt, spts, tpts, c_rad,  src_in_tree, ids, prefix):
 
                     f_tseg.write("\n".join(map(str, tpts.seg[cl.astype('int')])))
                     f_sseg.write("\n".join(map(str, [spts.seg[l].astype('int') for i in range(len(cl))] )))
-                #need to attach one more line here or we get two elements per line 
+                #need to attach one more line here or we get two elements per line
                 f_dis.write("\n")
                 f_src.write("\n")
                 f_tar.write("\n")
                 f_segs.write("\n")
 
-
     return [ids[0], res]
-
-
-
-
-
-
