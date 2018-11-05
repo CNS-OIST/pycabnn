5f68adc074afb2f3959e0f5d3cdc936de160695d
Alexji9494 <34013395+Alexji9494@users.noreply.github.com>
2018-11-05 11:46:45 +0900

    Update poisson_disk_sampling/matlabtopython.py
    Almostly converting the poisson matlab file to python script.

--- poisson_disk_sampling/matlabtopython.py
+++ poisson_disk_sampling/matlabtopython.py
@@ -4,113 +4,105 @@
 # More information at https://github.com/rougier/numpy-book
 # -----------------------------------------------------------------------------
 import numpy as np
+from scipy import spatial
+
 # 3D version
 
-def Bridson_sampling(sizel=(2, 2, 2), radius=0.005, k=42874):
+def Bridson_sampling(sizeI=(2, 2, 2), radius=0.005, showIter=0):
     # References: Fast Poisson Disk Sampling in Arbitrary Dimensions
     #             Robert Bridson, SIGGRAPH, 2007
 
-    # Here `3` corresponds to the number of dimension
-    ndim = len(sizel)
-    cellsize = radius/np.sqrt(ndim)
+    #Parsing inputs and setting default values
+    if nargin == 3:
+        showIter = 0
+    if nargin == 2:
+        showIter = 0
+        nPts = 0
+
+    #Setting properties of iteration
+    ndim = len(sizeI)
+    cellsize = radius / np.sqrt(ndim)
     k = 5
     dartFactor = 4
-    #rows = int(np.ceil(width/cellsize))
-    #cols = int(np.ceil(depth/cellsize))
-    #third = int(np.ceil(height/cellsize))
 
-    #Make a grid
-    for i in range(1, 3):
-        Grid[i] = np.array(np.mgrid([0:sizel[i-1]))
+    #Make grid size such that there is just one pt in each grid
+    dm = radius/sqrt(dim)
 
-    # Positions cells
-    P = np.zeros((rows, cols, third, 3), dtype=np.float32)
-    M = np.zeros((rows, cols, third), dtype=bool)
+    # Make a grid and convert it into a nx3 array 계속 지켜봐야함
+    sGrid = np.mgrid[row0:row1:cellsize, col0:col1:cellsize, third0:third1:cellsize]
+    for i in range(1, ndim):
+        sGrid[i] = np.reshape(sGrid[i][:], (-1, 1))
+    sizeGrid = np.size(sGrid[0])
 
-    # Cubic radius because we'll compare cubic distance
-    cubic_radius = radius*radius*radius
+    #Thrown in a particular grid
+    emptyGrid = np.ones(Grid, dtype=bool)
+    nEmptyGrid = sum(emptyGrid)
+    scoreGrid = np.zeros(Grid)
 
-    def squared_distance(p0, p1):
-        return (p0[0]-p1[0])**2 + (p0[1]-p1[1])**2 + (p0[2]-p1[2])**2
+    # Darts to be thrown per iterations
+    if nPts == 0:
+        nPts = nEmptyGrid
+        ndarts = round(nEmptyGrid / dartFactor)
+    ndarts = round(nPts / dartFactor)
 
-    def random_point_around(p, k=1):
-        # WARNING: This is not uniform around p but we can live with it
-        R = np.random.uniform(radius, 2*radius, k)
-        T = np.random.uniform(0, 2*np.pi, k)
-        O = np.random.uniform(0, 2 * np.pi, k)
-        P = np.empty((k, 3))
-        P[:, 0] = p[0]+R*np.cos(T)*np.sin(O) #x axis
-        P[:, 1] = p[1]+R*np.sin(T)*np.sin(O) #y axis
-        P[:, 2] = p[2] + R*np.cos(T) # z axis
-        return P
+    # Initialize Parameters
+    ptsCreated = 0
+    pts = []
+    iter = 0
 
-    def in_limits(p):
-        return 0 <= p[0] < width and 0 <= p[1] < depth and 0 <= p[2] < height
+    # Start Iterative process
+    while ptsCreated < nPts and nEmptyGrid > 0:
+        # Thrown darts in eligible grids
+        availGrid = np.where(emptyGrid == 1)
+        dataPts = min(nEmptyGrid, ndarts)
+        p = np.random.choice(availGrid, dataPts, replace=False)
+        tempPts = sGrid[p, :] + dm * rand(len(p), ndim)
 
-    def neighborhood(shape, index, n):
-        row, col, third = index
-        row0, row1 = max(row-n, 0), min(row+n+1, shape[0])
-        col0, col1 = max(col-n, 0), min(col+n+1, shape[1])
-        third0, third1 = max(third-n, 0), min(third+n+1, shape[2])
-        I = np.stack(np.mgrid[row0:row1, col0:col1, third0:third1], axis=3)
-        I = I.reshape(I.size//3, 3).tolist()
-        I.remove([row, col, third])
-        return I
+        # Find good dart throws
+        tree = spatial.KDtree([np.vstack(pts, tempPts)], tempPts, k=2)
+        tree = tree[:, 1]
 
-    def in_neighborhood(p):
-        i, j, u = int(p[0]/cellsize), int(p[1]/cellsize), int(p[2]/cellsize)
-        if M[i, j, u]:
-            return True
-        for (i, j, u) in N[(i, j, u)]:
-            if M[i, j, u] and squared_distance(p, P[i, j, u]) < cubic_radius :
-                return True
-        return False
+        withinI = logical(2 * (min(tempPts, sizel)))  # 이거 여쭈어보기
+        eligiblePts = withinI and tree > spacing
 
-    def add_point(p):
-        points.append(p)
-        i, j, u = int(p[0]/cellsize), int(p[1]/cellsize), int(p[2]/cellsize)
-        P[i, j, u], M[i, j, u] = p, True
+        scorePts = tempPts[~eligiblePts, :]
+        tempPts = temepPts[eligiblePts, :]
 
-    # Cache generation for neighborhood
-    N = {}
-    for i in range(rows):
-        for j in range(cols):
-            for u in range(third):
-                N[(i, j, u)] = neighborhood(M.shape, (i, j, u), 3)
+        # Update empty Grid
+        emptyPts = np.floor((tempPts + dm - 1) / dm)
+        emptyPts_0 = emptyPts[0]
+        emptyPts_1 = emptyPts[1]
+        emptyPts_2 = emptyPts[2]
+        emptyPts = (emptyPts_0, emptyPts_1, emptyPts_2)
+        emptyIdx = np.ravel(sizeGrid, emptyPts)  # 이거 좀 더 유심히
+        emptyGrid[emptyIdx] = 0
 
-    #Main process
-    points = []
-    add_point((width/2, depth/2, height/2))
-    count = 1
-    reject = 0
-    reject_count = []
-    count_count = []
-    elapsed_time = []
-    count_time = []
-    #theta = []
-    while (count < k):
-        #if theta < 0.8:
-            l = np.random.randint(count)
-            p = points[l]
-            Q = random_point_around(p, 30)
-            for q in Q:
-                if in_limits(q) and not in_neighborhood(q):
-                    add_point(q)
-                    # t1_end = time.perf_counter()
-                    print(count)
-                    print(q)
-                    count += 1
-                    end_time = time.perf_counter()
-                    # t1_start = time.perf_counter()
-                    elapsed_time.append(np.abs(end_time - start_time))
-                    count_time.append(count)
-                else:
-                    reject += 1
-                    reject_count.append(reject)
-                    count_count.append(count)
-                    #dx = np.diff(count_count)
-                    #dy = np.diff(reject_count)
-                    #theta = np.arctan(dy/dx)
-       # else:
-            #break
-    return P[M], count_count, reject_count, count_time, elapsed_time, width, depth, height, count
+        # Update score pts
+        scorePts = floor((scorePts + dm - 1) / dm)
+        scorePts_0 = scorePts[0]
+        scorePts_1 = scorePts[1]
+        scorePts_2 = scorePts[2]
+        scorePts = [scorePts_0, scorePts_1, scorePts_2]
+        scoreIdx = np.ravel(sizeGrid, scorePts)
+        scoreGrid[scoreIdx]= scoreGrid([scoreIdx] + 1
+
+        # Update emptyGrid if scoreGrid has exceeded k dart throws
+        emptyGrid1 = np.intersect1d(emptyGrid, (scoreGrid < k)) #왜 에러가 날까?
+
+        # Update quantities for next iterations
+        nEmptyGrid = sum(emptyGrid1)
+        pts = np.vstack(pts, tempPts)
+        ptsCreated = size(pts, 1)
+        iter += 1
+    end
+
+    # Cut down pts if more points are generated
+    if np.size(pts, 1) > nPts:
+        p = np.arange(1, size(pts, 1)+1)
+        p = np.random.choice(p, nPts, replace=False)
+        pts = pts[p, :]
+
+    return pts
+    if __name__ == '__main__':
+        points1 = Bridson_sampling()
+    print(points1)
