f09e5e18069121928ce5ed7f400bcf61a02b7484
Sungho Hong <shhong@oist.jp>
2019-01-13 13:15:51 +0900

    Make run_pybrep.py as a proper commandline app

--- run_pyBrep.py
+++ run_pyBrep.py
@@ -1,152 +1,221 @@
-import os
+#!/usr/bin/env python
+"""run_pybrep.py
+
+Jobs = AAtoGoC, PFtoGoC, GoCtoGoC, GoCtoGoCgap
+
+Usage:
+  run_pybrep.py (-i PATH) (-o PATH) (-p PATH) [--parallel] (all | <jobs>...)
+  run_pybrep.py (-h | --help)
+  run_pybrep.py --version
+
+Options:
+  -h --help                            Show this screen.
+  --version                            Show version.
+  -i PATH, --input_path=<input_path>   Input path.
+  -o PATH, --output_path=<output_path> Output path.
+  -p PATH, --param_path=<param_dir>    Params path.
+  --parallel                           Run things in parallel (via ipyparallel)
+
+"""
+
 from pathlib import Path
 import pybrep as brp
-import numpy as np
 import time
-from tqdm import tqdm
+from docopt import docopt
+from neuron import h
 
 
-######### Please feed with data!!
-#Parameters(might be read in from the command line some day...)
-#Output path
-input_path = Path("/Users/shhong/Dropbox/network_data/input_brep_2")
-output_path = input_path.parent / "output_pybrep_2r"
-# config files: if you work in an environment where you want
-paramdir = input_path.parent / 'model/params/set3005'
-config_hoc = paramdir / 'Parameters.hoc'
-config_pseudo_hoc = Path.cwd() / 'pseudo_hoc.pkl'
-# Coordinate input files
-gol_in = input_path / 'GoCcoordinates.dat'
-gran_in = input_path / 'GCcoordinates.dat'
-#smaller dataset
-#gol_in = './input_data/subsampled/GoCcoordinates_16.dat'
-#gran_in = './input_data/subsampled/GCcoordinates_16.dat'
+def load_input_data(args):
+    data = {}
+    for arg in args:
+        if 'path' in arg:
+            data[arg.replace('--', '')] = Path(args[arg]).resolve()
+
+    data['config_hoc'] = data['param_path'] / 'Parameters.hoc'
+
+    t1 = time.time()
+    print('Starting parallel process...')
+
+    # Read in the config file(or the pseudo-hoc, see pseudo-hoc class to find out how it is generated)
+    try:
+        from neuron import h
+        config_hoc = str(data['config_hoc'])
+        h.xopen(config_hoc)
+        print('Trying to read in hoc config object from ', config_hoc)
+    except ModuleNotFoundError:
+        # TODO: have to find a good way to deal with this
+        config_pseudo_hoc = Path.cwd() / 'pseudo_hoc.pkl'
+        h = brp.Pseudo_hoc(config_pseudo_hoc)
+        print('Trying to read in pseudo-hoc config object from ', config_pseudo_hoc)
+    finally:
+        #Just pick a random variable and check whether it is read
+        assert hasattr(h, 'GoC_Atheta_min'), 'There might have been a problem reading in the parameters!'
+        print('Succesfully read in config file!')
+
+    t2 = time.time()
+    print('Import finished:', t2-t1)
+    data['t'] = t2
+    return data
 
 
-########  IMPORTS(probably you don't have to change anything here...)
+def make_population(data):
+    output_path = data['output_path']
 
-t1 = time.time()
-print('Starting parallel process...')
+    # Set up the Golgi population, render dendrites
+    gol_in = data['input_path'] / 'GoCcoordinates.dat'
+    gg = brp.create_population('Golgi', h)
+    gg.load_somata(gol_in)
+    # # gg.gen_random_cell_loc(1995, 1500, 700, 200)
+    gg.add_dendrites()
+    gg.save_dend_coords(output_path)
+    gg.add_axon()
+    gg.save_somata(output_path, 'GoCcoordinates.sorted.dat')
 
-np.random.seed(0)
+    t2 = data['t']
+    t3 = time.time()
+    print('Golgi cell processing:', t3-t2)
 
-# Read in the config file(or the pseudo-hoc, see pseudo-hoc class to find out how it is generated)
-try:
-    import neuron
-    h = neuron.hoc.HocObject()
-    neuron.h.xopen(str(config_hoc))
-    print('Trying to read in hoc config object from ', config_hoc)
-except ModuleNotFoundError:
-    h = brp.Pseudo_hoc(config_pseudo_hoc)
-    print('Trying to read in pseudo-hoc config object from ', config_pseudo_hoc)
-finally:
-    #Just pick a random variable and check whether it is read
-    assert hasattr(h, 'GoC_Atheta_min'), 'There might have been a problem reading in the parameters!'
-    print('Succesfully read in config file!')
+    #Set up Granule population including aa and pf
 
+    gran_in = data['input_path'] / 'GCcoordinates.dat'
+    gp = brp.create_population('Granule', h)
+    gp.load_somata(gran_in)
+    gp.add_aa_endpoints_fixed()
+    gp.add_pf_endpoints()
+    gp.save_gct_points(output_path)
+    gp.save_somata(output_path, 'GCcoordinates.sorted.dat')
 
-t2 = time.time()
-print('Import finished:', t2-t1)
+    t4 = time.time()
+    print('Granule cell processing:', t4-t3)
+    print(' ')
 
+    data['t'] = t4
+    data['pops'] = {'grc': gp, 'goc': gg}
 
-# # ######### POPULATION SETUP(to do here: change to random generation of cells)
-
-# Set up the Golgi population, render dendrites
-gg = brp.create_population('Golgi', h)
-gg.load_somata(gol_in)
-# # gg.gen_random_cell_loc(1995, 1500, 700, 200)
-gg.add_dendrites()
-gg.save_dend_coords(output_path)
-gg.save_somata(output_path, 'GoCcoordinates.sorted.dat')
-
-t3 = time.time()
-print('Golgi cell processing:', t3-t2)
+    return data
 
 
-#Set up Granule population including aa and pf
-gp = brp.create_population('Granule', h)
-gp.load_somata(gran_in)
-#gp.gen_random_cell_loc(798000, 1500, 700, 200)
-gp.add_aa_endpoints_fixed()
-gp.add_pf_endpoints()
-gp.save_gct_points(output_path)
-gp.save_somata(output_path, 'GCcoordinates.sorted.dat')
+def run_AAtoGoC(data):
+    # you might want to change the radii
+    c_rad_aa = h.AAtoGoCzone/1.73
+    print("R for AA: {}".format(c_rad_aa))
 
-t4= time.time()
-print('Granule cell processing:', t4-t3)
-print(' ')
+    gp, gg = data['pops']['grc'], data['pops']['goc']
+    cc = brp.Connect_2D(gp.qpts_aa, gg.qpts, c_rad_aa)
+    # TODO: adapt the following from command line options
+    cc.connections_parallel(deparallelize=False, nblocks=120, debug=False)
+    cc.save_result( data['output_path'] / 'AAtoGoC')
 
-
-########## CONNECTIONS
-
-# you might want to change the radii
-c_rad_aa = h.AAtoGoCzone/1.73
-print("R for AA: {}".format(c_rad_aa))
-
-cc = brp.Connect_2D(gp.qpts_aa, gg.qpts, c_rad_aa)
-cc.connections_parallel(deparallelize=False, nblocks=120, debug=False)
-cc.save_result(output_path / 'AAtoGoC')
-t5 = time.time()
-print('AA: Found and saved after', t5-t4)
-print(' ')
-
-c_rad_pf = h.PFtoGoCzone/1.113
-print("R for PF: {}".format(c_rad_pf))
-
-cc = brp.Connect_2D(gp.qpts_pf, gg.qpts, c_rad_pf)
-cc.connections_parallel(deparallelize=False, nblocks=120, debug=False)
-cc.save_result(output_path / 'PFtoGoC')
-t6 = time.time()
-print('PF: Found and saved after', t5-t4)
-print(' ')
+    t5 = time.time()
+    t4 = data['t']
+    print('AA: Found and saved after', t5-t4)
+    print(' ')
+    data['t'] = t5
+    return data
 
 
-##### GoC-to-GoC connections
-##### TODO: need to be reimplemented by pybrep
-from sklearn.neighbors import KDTree
+def run_PFtoGoC(data):
+    c_rad_pf = h.PFtoGoCzone/1.113
+    print("R for PF: {}".format(c_rad_pf))
 
-gg.add_axon()
-naxon = len(gg.axon[0])-1
-dist = []
-src = []
-tgt = []
+    gp, gg = data['pops']['grc'], data['pops']['goc']
+    cc = brp.Connect_2D(gp.qpts_pf, gg.qpts, c_rad_pf)
+    cc.connections_parallel(deparallelize=False, nblocks=120, debug=False)
+    cc.save_result(data['output_path'] / 'PFtoGoC')
 
-for i in tqdm(range(gg.n_cell)):
-    axon_coord1 = gg.axon[i]
-    tree = KDTree(axon_coord1)
-    for j in range(gg.n_cell):
-        if i != j:
-            ii, di = tree.query_radius(np.expand_dims(gg.som[j], axis=0),
-                                       r=h.GoCtoGoCzone,
-                                       return_distance=True)
-            if ii[0].size > 0:
-                temp = di[0].argmin()
-                ii, di = ii[0][temp], di[0][temp]
-                axon_len = np.linalg.norm(axon_coord1[ii]-gg.som[i])
-                src.append(i)
-                tgt.append(j)
-                dist.append(axon_len + di) # putative path length along the axon
-
-np.savetxt(output_path / 'GoCtoGoCsources.dat', src, fmt='%d')
-np.savetxt(output_path / 'GoCtoGoCtargets.dat', tgt, fmt='%d')
-np.savetxt(output_path / 'GoCtoGoCdistances.dat', dist)
+    t6 = time.time()
+    t5 = data['t']
+    print('PF: Found and saved after', t6-t5)
+    print(' ')
+    data['t'] = t6
+    return data
 
 
-##### GoC-to-GoC GAP connections
+def run_GoCtoGoC(data):
+    import numpy as np
+    from tqdm import tqdm
 
-dist = []
-src = []
-tgt = []
+    ##### TODO: need to be reimplemented by pybrep
+    from sklearn.neighbors import KDTree
 
-for i in tqdm(range(gg.n_cell)):
-    for j in range(gg.n_cell):
-        if i != j:
-            di = np.linalg.norm(gg.som[j]-gg.som[i])
-            if di < h.GoCtoGoCgapzone:
-                src.append(i)
-                tgt.append(j)
-                dist.append(di)
+    gg = data['pops']['goc']
+    dist = []
+    src = []
+    tgt = []
 
-np.savetxt(output_path / 'GoCtoGoCgapsources.dat', src, fmt='%d')
-np.savetxt(output_path / 'GoCtoGoCgaptargets.dat', tgt, fmt='%d')
-np.savetxt(output_path / 'GoCtoGoCgapdistances.dat', dist)
+    for i in tqdm(range(gg.n_cell)):
+        axon_coord1 = gg.axon[i]
+        tree = KDTree(axon_coord1)
+        for j in range(gg.n_cell):
+            if i != j:
+                ii, di = tree.query_radius(np.expand_dims(gg.som[j], axis=0),
+                                           r=h.GoCtoGoCzone,
+                                           return_distance=True)
+                if ii[0].size > 0:
+                    temp = di[0].argmin()
+                    ii, di = ii[0][temp], di[0][temp]
+                    axon_len = np.linalg.norm(axon_coord1[ii]-gg.som[i])
+                    src.append(i)
+                    tgt.append(j)
+                    dist.append(axon_len + di) # putative path length along the axon
+
+    output_path = data['output_path']
+
+    np.savetxt(output_path / 'GoCtoGoCsources.dat', src, fmt='%d')
+    np.savetxt(output_path / 'GoCtoGoCtargets.dat', tgt, fmt='%d')
+    np.savetxt(output_path / 'GoCtoGoCdistances.dat', dist)
+
+    t2 = time.time()
+    t1 = data['t']
+    print('GoCtoGoC: Found and saved after', t2-t1)
+    data['t'] = t2
+    return data
+
+
+def run_GoCtoGoCgap(data):
+    import numpy as np
+    from tqdm import tqdm
+
+    gg = data['pops']['goc']
+    dist = []
+    src = []
+    tgt = []
+
+    for i in tqdm(range(gg.n_cell)):
+        for j in range(gg.n_cell):
+            if i != j:
+                di = np.linalg.norm(gg.som[j]-gg.som[i])
+                if di < h.GoCtoGoCzone:
+                    src.append(i)
+                    tgt.append(j)
+                    dist.append(di)
+
+    output_path = data['output_path']
+    np.savetxt(output_path / 'GoCtoGoCgapsources.dat', src, fmt='%d')
+    np.savetxt(output_path / 'GoCtoGoCgaptargets.dat', tgt, fmt='%d')
+    np.savetxt(output_path / 'GoCtoGoCgapdistances.dat', dist)
+
+    t2 = time.time()
+    t1 = data['t']
+    print('GoCtoGoCgap: Found and saved after', t2-t1)
+    data['t'] = t2
+    return data
+
+
+def main(args):
+    data = load_input_data(args)
+    data = make_population(data)
+
+    valid_job_list = ['AAtoGoC', 'PFtoGoC', 'GoCtoGoC', 'GoCtoGoCgap']
+    if args['all']:
+        args['<jobs>'] = valid_job_list
+    for j in args['<jobs>']:
+        if j not in valid_job_list:
+            raise RuntimeError('Job {} is not valid, not in {}'.format(j, valid_job_list))
+        else:
+            data = eval('run_'+j)(data)
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__, version='0.7dev')
+    main(args)
