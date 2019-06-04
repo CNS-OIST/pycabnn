acc8d15be50f96d2ad107820d934701da2437054
Sungho@macpro <shhong@oist.jp>
2019-06-04 20:11:10 +0900

    Test different ways to plot cell positions

--- docs/fig_POPGEN/pds_plots.py
+++ docs/fig_POPGEN/pds_plots.py
@@ -1,35 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
+from tqdm.autonotebook import tqdm
 
-def plot_circles(mf_points, box, r, color, ax=None):
+plt.style.use('dark_background')
+
+def plot_circles(mf_points, box, r, color, ec=None, ax=None):
     xy = mf_points-np.array([25, 25])
     Horizontal_range, Transverse_range = box
 
     if ax is None:
-        _, ax = plt.subplots(figsize=(14, 7))
+        bboxr = (box[1][1]-box[1][0])/(box[0][1]-box[0][0])
+        _ = plt.figure(figsize=(8.5, 8.5*bboxr))
+        ax = plt.subplot(111)
 
     if type(r)==np.ndarray:
-        for i, p in enumerate(mf_points):
-            ax.add_artist(Circle((p[0], p[1]), radius=r[i], color=color))
+        for i, p in tqdm(enumerate(mf_points)):
+            ax.add_artist(Circle((p[0], p[1]), radius=r[i], facecolor=color, edgecolor=ec))
     else:
-        for p in mf_points:
-            ax.add_artist(Circle((p[0], p[1]), radius=r, color=color))
+        for p in tqdm(mf_points):
+            ax.add_artist(Circle((p[0], p[1]), radius=r, facecolor=color, edgecolor=ec))
     # ax.scatter(mf_points[:, 0], mf_points[:, 1], 50, 'k')
-    ax.set(xlim=[0, Horizontal_range],
-           ylim=[0, Transverse_range])
+
+    ax.set(xlim=Horizontal_range,
+           ylim=Transverse_range,
+           xlabel=None, ylabel=None, xticks=[], yticks=[])
 
     plt.axis('off')
-    plt.tight_layout()
+
+    plt.subplots_adjust(
+        left=0.0, right=1.0, top=1.0, bottom=0.0,
+    )
 
     return ax
 
 
 def plot_mf_1(mf_points, box, r, save=False):
-    plot_circles(mf_points, box, r, color='k')
-    if save:
-        plt.savefig('mf.png', dpi=300)
-    # plt.show()
+    ax = plot_circles(mf_points, box, r, color='w')
+
+    return ax
 
 
 def plot_mf_2(mf_points, box, save=False):
@@ -47,49 +56,45 @@ def plot_mf_2(mf_points, box, save=False):
         plt.savefig('mf_vor.png', dpi=300)
 
 
-def plot_slice(points, box, z_focal, r, color='k', ax=None):
+def plot_slice(points, box, z_focal, r, color='w', ec=None, ax=None):
     z = points[:, 2]
     zdist = np.abs(z-z_focal)
     indc = (zdist < r)
     spoints = points[indc, :]
     r_focal = np.sqrt(r**2 - zdist[indc]**2)
-    return plot_circles(spoints[:,:2], box[:2], r_focal, color=color, ax=ax)
+    return plot_circles(spoints[:,:2], box[:2], r_focal, color=color, ec=ec, ax=ax)
 
 
-def plot_goc(points, box, slice, r, save=False, ax=None):
-    _ = plot_slice(points, box, slice, r)
-    if save:
-        plt.savefig('goc.png', dpi=300)
+def plot_goc(points, box, slice, r, ax=None):
+    return plot_slice(points, box, slice, r, color='grey', ax=ax)
     # plt.show()
 
 
-def plot_glo(points, box, slice, r, save=False):
-    _ = plot_slice(points, box, slice, r)
-    if save:
-        plt.savefig('glo.png', dpi=300)
+def plot_glo(points, box, slice, r, ax=None):
+    return plot_slice(points, box, slice, r, color='y', ax=ax)
+    # plt.show()
+
+def plot_grc(points, box, slice, r, ax=None):
+    return plot_slice(points, box, slice, r, color='b', ec='r', ax=ax)
     # plt.show()
 
 
-def plot_goc_glo(points_r1, points_r2, box, slice, save=False):
+def plot_goc_glo(points_r1, points_r2, box, slice):
     points, r = points_r1
-    ax = plot_slice(points, box, slice, r, color='g')
+    ax = plot_goc(points, box, slice, r)
 
     points, r = points_r2
-    _ = plot_slice(points, box, slice, r, color='m', ax=ax)
-    if save:
-        plt.savefig('goc+glo.png', dpi=300)
-    # plt.show()
+    return plot_glo(points, box, slice, r, ax=ax)
 
-def plot_all_pop(points_r1, points_r2, points_r3, box, slice, save=False):
+
+def plot_all_pop(points_r1, points_r2, points_r3, box, slice):
     points, r = points_r1
-    ax = plot_slice(points, box, slice, r, color='grey')
+    ax = plot_goc(points, box, slice, r)
 
     points, r = points_r2
-    ax = plot_slice(points, box, slice, r, color='r', ax=ax)
+    ax = plot_glo(points, box, slice, r, ax=ax)
 
     points, r = points_r3
-    _ = plot_slice(points, box, slice, r, color='k', ax=ax)
+    return plot_grc(points, box, slice, r, ax=ax)
 
-    if save:
-        plt.savefig('goc+glo+grc.png', dpi=300)
     # plt.show()
--- docs/fig_POPGEN/plot_pop_gen_figures.py
+++ docs/fig_POPGEN/plot_pop_gen_figures.py
@@ -1,36 +1,99 @@
 # %%
+%load_ext autoreload
+%autoreload 2
+%matplotlib inline
+
 import pds_plots as ppl
 
 # %%
+import numpy as np
 
-ppl.plot_mf_1(mf_points, [1500, 750], 4, save=True)
-ppl.plot_mf_2(mf_points, [1500, 750], save=True)
+f = np.load('coords_20190604_1.npz')
+
+mf = f['mf']
+goc = f['goc']
+glo = f['glo']
+grc = f['grc_nop']
+
+bbox = [[600,800], [200-20, 400], [0, 200]]
+
+def limit_to_box(x, box):
+    mf = x.copy()
+    for i, t in enumerate(box):
+        mf = mf[mf[:,i]>=t[0], :]
+        mf = mf[mf[:,i]<=t[1], :]
+    return mf
+
+def fix_coors(x):
+    y = x.copy()
+    y = y-np.ones(y.shape[1])*25
+    box = [bbox[0], [bbox[1][0]+20, bbox[1][1]], bbox[2]]
+    return limit_to_box(y, box[:x.shape[1]])
+
+mf = fix_coors(mf)
+goc = fix_coors(goc)
+glo = fix_coors(glo)
+grc = fix_coors(grc)
+
+# %%
+bbox = bbox[:2]
+ax = ppl.plot_mf_1(mf, bbox, 4, save=False)
+ax.plot([bbox[0][1]-100, bbox[0][1]], [bbox[1][0], bbox[1][0]], 'w', linewidth=15)
+# ppl.plot_mf_2(mf, [1500, 700], save=True)
+# plt.savefig('mf.png', dpi=300/2.54)
 
 
 # %%
-ppl.plot_goc(goc_points, [1500, 750], 100, 16, save=True)
-
-ppl.plot_glo(glo_points, [1500, 750], 50, 3.3)
-
-# %%
-ppl.plot_goc_glo((goc_points, 15), (glo_points, 6.6 / 2), [1500, 750], 100, save=True)
-plt.xlim([25, 425])
-plt.ylim([25, 225])
+ax = ppl.plot_goc(goc, bbox, 100, 17)
+ax.plot([bbox[0][1]-100, bbox[0][1]], [bbox[1][0], bbox[1][0]], 'w', linewidth=15)
 
 # %%
-ppl.plot_goc_glo((glo_points, 6.6 / 1.75), (grc_points, 3), [1500, 750], 100)
-plt.xlim([25, 425])
-plt.ylim([25, 225])
+ax = ppl.plot_goc_glo((goc, 17), (glo, 6.6 / 1.75), bbox, 100)
+ax.plot([bbox[0][1]-100, bbox[0][1]], [bbox[1][0], bbox[1][0]], 'w', linewidth=15)
+
+#         plt.savefig('goc+glo.png', dpi=300
 
 # %%
-ppl.plot_all_pop(
-    (goc_points, 17),
-    (glo_points, 6.6 / 1.75),
-    (grc_points, 3),
-    [1500, 750],
-    100,
-    save=True,
-)
-plt.xlim([25, 425])
-plt.ylim([25, 225])
+ax = ppl.plot_all_pop(
+    (goc, 17),
+    (glo, 6.6 / 1.75),
+    (grc, 2.75),
+    bbox, 100)
+ax.plot([bbox[0][1]-100, bbox[0][1]], [bbox[1][0], bbox[1][0]], 'w', linewidth=15)
 
+
+
+# %%
+def to_yzx(x):
+    y = x.copy()
+    y[:,0] = x[:,1]
+    y[:,1] = x[:,2]
+    y[:,2] = x[:,0]
+    return y
+
+goc = to_yzx(goc)
+glo = to_yzx(glo)
+grc = to_yzx(grc)
+
+bbox = [[200,400], [0-20, 200]]
+ax = ppl.plot_goc(goc, bbox, 700, 17)
+ax.plot([bbox[0][1]-100, bbox[0][1]], [bbox[1][0], bbox[1][0]], 'w', linewidth=15)
+
+# %%
+ax = ppl.plot_goc_glo((goc, 17), (glo, 6.6 / 1.75), bbox, 700)
+ax.plot([bbox[0][1]-100, bbox[0][1]], [bbox[1][0], bbox[1][0]], 'w', linewidth=15)
+
+# %%
+ax = ppl.plot_all_pop(
+    (goc, 17),
+    (glo, 6.6/1.55),
+    (grc, 3),
+    bbox, 700)
+ax.plot([bbox[0][1]-100, bbox[0][1]], [bbox[1][0], bbox[1][0]], 'w', linewidth=15)
+
+
+# %%
+
+# %%
+
+# %%
