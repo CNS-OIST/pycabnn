b4a106d924fa443a7e9e2b3be2a188c9870b6e29
Sungho Hong <shhong@oist.jp>
2019-06-25 16:36:11 +0900

    Add files for computing the glomerulus-to-GrC connectivity

---
+++ docs/fig_GLO_GRC/fig_GLO_GRC.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.1.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # Glomerulus - GrC connectivity
+
+
