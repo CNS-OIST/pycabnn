9e3a1393a54c42a55ad415eddce5b66688f8764e
Sungho Hong <shhong@oist.jp>
2019-10-22 18:07:54 +0900

    Add the Purkinje cell generation

---
+++ docs/fig_PC/PC_test.py
@@ -0,0 +1,86 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:hydrogen
+#     text_representation:
+#       extension: .py
+#       format_name: hydrogen
+#       format_version: '1.2'
+#       jupytext_version: 1.2.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import sys
+sys.path.append('../../')
+
+# %%
+import numpy as np
+from neuron import h
+from pybrep.pop_generation.ebeida import ebeida_sampling
+from pybrep.pop_generation.bridson import Bridson_sampling_2d
+from pybrep.pop_generation.utils import PointCloud
+import matplotlib.pyplot as plt
+
+# %%
+h.load_file("../../test/set3005/Parameters.hoc")
+h.MFxrange = 200
+h.MFyrange = 200
+h.GLdepth += 50
+
+def compute_mf_params(h):
+    Transverse_range = h.MFyrange
+    Horizontal_range = h.MFxrange
+    Vertical_range = h.GLdepth
+    Volume = Transverse_range * Horizontal_range * Vertical_range
+
+    MFdensity = h.MFdensity
+
+    box_fac = 2.5
+    Xinstantiate = 64 + 40  # 297+40
+    Yinstantiate = 84 + 40 * box_fac  # 474+40x*box_fac
+
+    n_mf = int(
+        (Transverse_range + (2 * Xinstantiate))
+        * (Horizontal_range + (2 * Yinstantiate))
+        * MFdensity
+        * 1e-6
+    )
+    print("N MF = {}".format(n_mf))
+    return (
+        (Horizontal_range + (2 * Yinstantiate), Transverse_range + (2 * Xinstantiate)),
+        n_mf,
+    )
+
+
+mf_box, n_mf = compute_mf_params(h)
+mf_box = (200, 200)
+
+# %%
+spacing_mf = 20
+
+mf_points = ebeida_sampling(mf_box, spacing_mf, n_mf+500, True)
+# mf_points = Bridson_sampling_2d(mf_box, spacing_mf, 82, True)
+
+# mf_points = mf_points + np.random.randn(mf_points.shape[0], mf_points.shape[1])*0
+
+# %%
+x = mf_points
+_, ax = plt.subplots(ncols=2, figsize=(16,8))
+
+ax[0].scatter(x[:,0], x[:,1], 100)
+
+ax[1].scatter(x[:,0], x[:,1], 1000)
+ax[1].scatter(x[:,0], x[:,1], 50)
+
+for i in range(12):
+    ax[1].plot([0, h.MFxrange+20], [20*i, 20*i], 'k')
+for i in range(12):
+    ax[1].plot([20*i, 20*i], [0, h.MFyrange+20], 'k')
+
+
+
+# %%
---
+++ docs/fig_PC/pds_plots.py
@@ -0,0 +1,100 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+from tqdm.autonotebook import tqdm
+
+# plt.style.use('dark_background')
+
+def plot_circles(mf_points, box, r, color, ec=None, ax=None):
+    xy = mf_points-np.array([25, 25])
+    Horizontal_range, Transverse_range = box
+
+    if ax is None:
+        bboxr = (box[1][1]-box[1][0])/(box[0][1]-box[0][0])
+        _ = plt.figure(figsize=(8.5, 8.5*bboxr))
+        ax = plt.subplot(111)
+
+    if type(r)==np.ndarray:
+        for i, p in tqdm(enumerate(mf_points)):
+            ax.add_artist(Circle((p[0], p[1]), radius=r[i], facecolor=color, edgecolor=ec, linewidth=0.5))
+    else:
+        for p in tqdm(mf_points):
+            ax.add_artist(Circle((p[0], p[1]), radius=r, facecolor=color, edgecolor=ec, linewidth=0.5))
+    # ax.scatter(mf_points[:, 0], mf_points[:, 1], 50, 'k')
+
+    ax.set(xlim=Horizontal_range,
+           ylim=Transverse_range,
+           xlabel=None, ylabel=None, xticks=[], yticks=[])
+
+    plt.axis('off')
+
+    plt.subplots_adjust(
+        left=0.0, right=1.0, top=1.0, bottom=0.0,
+    )
+
+    return ax
+
+
+def plot_mf_1(mf_points, box, r, save=False):
+    ax = plot_circles(mf_points, box, r, color='k')
+
+    return ax
+
+
+def plot_mf_2(mf_points, box, save=False):
+    from scipy.spatial import Voronoi, voronoi_plot_2d
+
+    Horizontal_range, Transverse_range = box
+
+    vor = Voronoi(mf_points)
+    fig = voronoi_plot_2d(vor, linewidth=0.01, point_size=8, show_vertices=False)
+    plt.ylim([0, Transverse_range/4*1.75])
+    plt.xlim([0, Horizontal_range/4*1.75*0.75])
+    plt.axis('off')
+    plt.tight_layout()
+    if save:
+        plt.savefig('mf_vor.png', dpi=300)
+
+
+def plot_slice(points, box, z_focal, r, color='w', ec=None, ax=None):
+    z = points[:, 2]
+    zdist = np.abs(z-z_focal)
+    indc = (zdist < r)
+    spoints = points[indc, :]
+    r_focal = np.sqrt(r**2 - zdist[indc]**2)
+    return plot_circles(spoints[:,:2], box[:2], r_focal, color=color, ec=ec, ax=ax)
+
+
+def plot_goc(points, box, slice, r, ax=None):
+    return plot_slice(points, box, slice, r, color='grey', ax=ax)
+    # plt.show()
+
+
+def plot_glo(points, box, slice, r, ax=None):
+    return plot_slice(points, box, slice, r, color='y', ax=ax)
+    # plt.show()
+
+def plot_grc(points, box, slice, r, ax=None):
+    return plot_slice(points, box, slice, r, color='b', ec='r', ax=ax)
+    # plt.show()
+
+
+def plot_goc_glo(points_r1, points_r2, box, slice):
+    points, r = points_r1
+    ax = plot_goc(points, box, slice, r)
+
+    points, r = points_r2
+    return plot_glo(points, box, slice, r, ax=ax)
+
+
+def plot_all_pop(points_r1, points_r2, points_r3, box, slice):
+    points, r = points_r1
+    ax = plot_goc(points, box, slice, r)
+
+    points, r = points_r2
+    ax = plot_glo(points, box, slice, r, ax=ax)
+
+    points, r = points_r3
+    return plot_grc(points, box, slice, r, ax=ax)
+
+    # plt.show()
