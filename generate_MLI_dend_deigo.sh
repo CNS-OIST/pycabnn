f9bcfba11f58bd6e0df366d82695c15c5bac42aa
Sungho@deigo <shhong@oist.jp>
2021-08-23 17:11:39 +0900

    Use nrngui -python instead of python in deigo

--- generate_MLI_dend_deigo.sh
+++ generate_MLI_dend_deigo.sh
@@ -2,17 +2,15 @@
 
 #SBATCH --job-name=MLI_dend_gen
 #SBATCH --partition=compute
-#SBATCH --mem-per-cpu=10g
+#SBATCH --mem-per-cpu=4g
 #SBATCH --time=04:00:00
 #SBATCH --ntasks=1
 #SBATCH --output=gen_mli_dend_gen.out.log
 #SBATCH --error=gen_mli_dend_gen.err.log
 
-NEURONHOME=/apps/unit/DeSchutterU/neuron/py3_7.7
+export NEURONHOME=/apps/unit/DeSchutterU/neuron/7.8.2
 export PATH=$NEURONHOME/x86_64/bin:$PATH
-export LD_LIBRARY_PATH=$NEURONHOME/x86_64/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$NEURONHOME/lib/python:$NEURONHOME/x86_64/lib:$LD_LIBRARY_PATH
 export PYTHONPATH=$NEURONHOME/lib/python:$PYTHONPATH
 
-which python
-python -c "import neuron"
-
+nrngui -python generate_MLI_dend.py
